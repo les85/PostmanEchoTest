e97871c38addc0196d36c72fecb17b8aaac16aa5
lesnoy <bigib@yandex.ru>
2024-04-26 14:24:25 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+    @Test
+    void ShouldReturnSendData() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+    }
+}
